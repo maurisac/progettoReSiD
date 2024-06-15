6a09bb260692ba093d1216d1ad331d784a844669
maurisac <mauriziosacc4gmail.com>
2024-06-15 20:31:52 +0200

    Aggiunto il file main che farà da cardine per il progetto

---
+++ main.py
@@ -0,0 +1,7 @@
+#codice principale in cui verranno eseguite le altre cose
+
+
+
+#per prima cosa c'è la connessione col client
+
+#autenticazione (con thread apposito che gestisce il file degli utenti con mutex) e poi menù di scelta per scegliere il video da riprodurre. Scelto il video, si avvia lo streaming
