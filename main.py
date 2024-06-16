3b52ade173dc07ec00d33445d998b9a11e1a9e71
maurisac <mauriziosacc4gmail.com>
2024-06-16 18:05:11 +0200

    Aggiunto sblocco del mutex e miglioramento alla leggibilità

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 #codice principale del server
-
-
-
 #per prima cosa c'è la connessione col client
 
 #autenticazione (con thread apposito che gestisce il file degli utenti con mutex) e poi menù di scelta per scegliere il video da riprodurre. Scelto il video, si avvia lo streaming
@@ -37,33 +34,47 @@ def handleClient(clientSock, clientAddr):
             clientSock.send(b"Arrivederci!.")
             clientSock.close()
             return
+        
+
         clientSock.send(b"Username: ")
         username = clientSock.recv(BUFFERSIZE).strip().decode('utf-8')
         
         clientSock.send(b"Password: ")
         password = clientSock.recv(BUFFERSIZE).strip().decode('utf-8')
 
+
         with MUTEX:
             if mode.lower() == 'registrazione':
                 if authuser.add_user(username, password) == False:
                     clientSock.send(b"Errore: registrazione fallita, Verrai disconnesso.")
+                    MUTEX.release()
                     return
                 clientSock.send(b"Registrazione effettuata con successo!")
+                MUTEX.release()
                 menu(username, clientSock, clientAddr)
             elif mode.lower() == 'accedi':
                 if not authuser.authenticate_user(username, password):
                     clientSock.send(b"Errore: autenticazione fallita, Verrai disconnesso.")
+                    MUTEX.release()
                     return
                 clientSock.send(b"Accesso effettuato con successo!")
+                MUTEX.release()
                 menu(username, clientSock, clientAddr)
             else:
                 clientSock.send(b"Operazione non valida. Disconnessione.")
+                MUTEX.release()
                 return
+            
     except Exception as e:
         print(f"[ERROR] Errore durante la gestione del client {clientAddr}: {e}")
+
+
     finally:
         clientSock.close()
         print(f"[INFO] Connessione chiusa con: {clientAddr}")
+
+
+
 
 def menu(username, clientSock, clientAddr):
     clientSock.sendall(f"\nBenvenuto {username}, inserisci l'operazione che vuoi effettuare:\n1: Streaming audio\n2: Esci\n".encode())
@@ -77,6 +88,9 @@ def menu(username, clientSock, clientAddr):
         else:
             clientSock.sendall(b"Operazione non valida. Riprova.")
 
+
+
+
 def streaming(username, clientSock, clientAddr):
     clientSock.sendall(b"Scegli l'ID dell'audio da riprodurre: ")
     with open("audios.txt", "r") as file:
@@ -86,6 +100,9 @@ def streaming(username, clientSock, clientAddr):
         chosenAudio = clientSock.recv(BUFFERSIZE).strip().decode('utf-8')
         # Logica di streaming (es. apertura finestra VLC, streaming, chiusura)
         clientSock.sendall(f"Audio scelto: {audios[int(chosenAudio)].strip()}".encode())
+
+
+
 
 while True:
     client_socket, client_address = server.accept()
